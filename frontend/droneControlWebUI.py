ac8b5292c3b96f5e03c4f64eab37d05090bd29d4
Mario Bartolome <mbm0089@alu.ubu.es>
2018-04-05 18:06:17 +0200

    Security layer added. #41
    SSL/TLS certificates requested to https://letsencrypt.org/
    
    Now the app is using a proper certificate to enable HTTPS without any warnings. Also the WebSockets connections are made through WSS.

--- frontend/droneControlWebUI.py
+++ frontend/droneControlWebUI.py
@@ -9,4 +9,4 @@ from frontend.controller import app, socketio
 
 
 if __name__ == '__main__':
-	socketio.run(app, log_output=True)
+	socketio.run(app, log_output=True, host='0.0.0.0', certfile='../private/fullchain.pem', keyfile='../private/privkey.pem')
