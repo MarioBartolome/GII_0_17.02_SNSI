460ddf38fc52af79a7014830219d6665f4e915a2
Mario Bartolome <mbm0089@alu.ubu.es>
2018-04-17 10:43:25 +0200

    Backend deployment implementation. #48
    This script allows to deploy the backend part of the project on a RPi-like system.
    
    It will create a virtual environment to store all the necessary Python packages. Closes #48

---
+++ setupBackend.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(filename):
+	return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+	name='GII_0_17.02_SNSI',
+	version='0.2.0a0',
+	author='Mario Bartolome',
+	author_email='mbm0089@alu.ubu.es',
+	long_description=read('README.md'),
+	packages=['backend'],
+	include_package_data=True,
+	install_requires=[
+		'pyserial',
+		'numpy',
+	],
+	zip_safe=False
+)
